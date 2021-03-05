892725699063101504c6089f8d2ba2806ae8cfc0
Fabrice Claeys <fabrice.claeys@symbol-it.fr>
2021-03-05 09:00:28 +0100

    correction dechiffrage cesar

--- cesar.js
+++ cesar.js
@@ -1,7 +1,10 @@
 const readlineSync = require('readline-sync');
 
+
+const alphabet = 'abcdefghijklmnopqrstuvwxyz ';
+
+
 function chiffrageDeCesar(phrase, decallage) {
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz ';
     let resultat='';
 
     for(let indexPhrase=0; indexPhrase < phrase.length; indexPhrase++) {
@@ -22,15 +25,43 @@ function chiffrageDeCesar(phrase, decallage) {
     return resultat
 }
 
+function dechiffrageDeCesar(phrase, decallage) {
+    let resultat='';
+
+    for(let indexPhrase=0; indexPhrase < phrase.length; indexPhrase++) {
+        const lettreCourante = phrase[indexPhrase];
+
+        for(let indexAlphabet = 0; indexAlphabet < alphabet.length; indexAlphabet ++){
+            if(lettreCourante === alphabet[indexAlphabet]){
+                let positionDecalage = indexAlphabet - decallage;
+                if( positionDecalage < 0) {
+                    positionDecalage = positionDecalage + alphabet.length
+                } 
+
+                resultat = resultat + alphabet[positionDecalage]
+            }
+        }
+    }
+
+    return resultat
+}
 
 function main() {
-
+    console.log('Taper 1 pour chiffrer.')
+    console.log('Taper 2 pour dechiffrer.')
+    const choix = readlineSync.questionInt('Quel est votre choix ?');
     const phrase = readlineSync.question('Veuillez saisir une phrase ?');
     const decalage = readlineSync.questionInt('Veuillez saisir un decalage ?');
 
-    const result = chiffrageDeCesar(phrase, decalage)
+    if (choix === 1){
 
-    console.log(result)
+        const result = chiffrageDeCesar(phrase, decalage)
+        console.log(result)
+    } else {
+        const result = dechiffrageDeCesar(phrase, decalage)
+        console.log(result)
+    }
+
 }
 
 main()
